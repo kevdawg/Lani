924a15950fab7515529c7baccee3917d5facd3e3
kevdawg <kevin@lanilabs.com>
2014-01-27 00:00:14 -0500

    first commit

---
+++ kevin/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kevin::Application.config.secret_key_base = '942ddb524196337be946e161fee91e8f7079abdb242c6d4480c3f091a93c0728e35e1091663ef8ebcf25ba693b8e6fa5bf010ff2d706967cb99f8c7a663634f1'
---
+++ kevin/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kevin::Application.config.session_store :cookie_store, key: '_kevin_session'
